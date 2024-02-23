d932cd69e4bfdbe4179e091d963e2320c1d479c7
mosimosidesu <mosimosidesu@gmail.com>
2024-02-23 20:20:40 +0900

    commit

--- led01.py
+++ led01.py
@@ -3,12 +3,17 @@ import gpiozero
 
 # GPIOピン番号を指定
 gpio_pin_number = 17
+# 待ち時間１
+wait_time1 = 0.5
+# 待ち時間２
+wait_time2 = 1
+
 
 # LEDを指定したGPIOピンに接続
 led = gpiozero.LED(gpio_pin_number)
 
 while True:
     led.on()  # LEDを点灯
-    time.sleep(1)  # 1秒待つ
+    time.sleep(wait_time2)  # 待つ
     led.off()  # LEDを消灯
-    time.sleep(1)  # 1秒待つ
+    time.sleep(wait_time1)  # 待つ
