c2ec38ff1248259172e5608f9dde5724df037fe8
mosimosidesu <mosimosidesu@gmail.com>
2024-02-23 13:37:58 +0900

    commit

--- led01.py
+++ led01.py
@@ -1,21 +1,23 @@
-import RPi.GPIO as GPIO  # RPi.GPIOライブラリをインポート
-import time              # timeライブラリをインポート
+import time
+import gpiod
 
-GPIO.setmode(GPIO.BCM)  # ピン番号をGPIO番号で指定することを宣言
+# GPIO番号を変数に設定
+gpio_num = 17
 
-LED_PIN = 17             # LEDが接続されているGPIOの番号。必要に応じて更可能。
+# チップの設定
+chip = gpiod.Chip('gpiochip0')
 
-GPIO.setup(LED_PIN, GPIO.OUT)  # LED_PINを出力モードに設定
+# ピンの設定
+line = chip.get_line(gpio_num)
+line.request(consumer='my_app', type=gpiod.LINE_REQ_DIR_OUT)
 
 try:
-    while True:                 # 無限ループ
-        GPIO.output(LED_PIN, GPIO.HIGH)  # LED_PINに高電圧を出力（LED点灯）
-        time.sleep(1)           # 1秒待つ
-
-        GPIO.output(LED_PIN, GPIO.LOW)   # LED_PINに低電圧を出力（LED消灯）
-        time.sleep(1)           # 1秒待つ
-
-except KeyboardInterrupt:       # CTRL+Cが押された場合の処理
-    pass                        # 何もしない
-
-GPIO.cleanup()                  # GPIO設定をリセット
+    while True:
+        line.set_value(1)  # LED点灯
+        time.sleep(1)  # 1秒間待つ
+        line.set_value(0)  # LED消灯
+        time.sleep(1)  # 1秒間待つ
+except KeyboardInterrupt:
+    pass
+finally:
+    line.release()
