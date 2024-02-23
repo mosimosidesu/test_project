fd7cfd202834bfe3053e85c8a3d7a853a1ad19ae
mosimosidesu <mosimosidesu@gmail.com>
2024-02-23 13:43:02 +0900

    commit

--- led01.py
+++ led01.py
@@ -1,23 +1,11 @@
 import time
-import gpiod
+import gpiozero
 
-# GPIO番号を変数に設定
-gpio_num = 17
+# LEDをGPIO18ピンに接続
+led = gpiozero.LED(17)
 
-# チップの設定
-chip = gpiod.Chip('gpiochip0')
-
-# ピンの設定
-line = chip.get_line(gpio_num)
-line.request(consumer='my_app', type=gpiod.LINE_REQ_DIR_OUT)
-
-try:
-    while True:
-        line.set_value(1)  # LED点灯
-        time.sleep(1)  # 1秒間待つ
-        line.set_value(0)  # LED消灯
-        time.sleep(1)  # 1秒間待つ
-except KeyboardInterrupt:
-    pass
-finally:
-    line.release()
+# while True:
+    led.on()  # LEDを点灯
+    time.sleep(1)  # 1秒待つ
+    led.off()  # LEDを消灯
+    time.sleep(1)  # 1秒待つ
